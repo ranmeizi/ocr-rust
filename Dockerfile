7efd4d5b02815b6948870e7766420ff0f201f78a
boboan <360969885@qq.com>
2023-11-22 17:12:59 +0800

    test: deploy

--- Dockerfile
+++ Dockerfile
@@ -5,11 +5,15 @@ COPY ocr-test/* /tmp/
 # 安装 ocr 环境
 RUN apt-get update ; apt-get install -y tesseract-ocr
 
-RUN tesseract /tmp/hrl1.jpg stdout
+# 安装 opencv 环境
+RUN apt-get update ; apt-get install -y clang libopencv-dev libclang-dev
+
+# 复制代码
+COPY . /app/
 
 # 启动 app 服务
 WORKDIR /app
 
-COPY pre-build/ocr-rust /app/
+RUN cargo build --release
 
-CMD /app/ocr-rust
+CMD /app/target/release/ocr-rust
--- src/services/ocr.rs
+++ src/services/ocr.rs
@@ -1,24 +1,24 @@
 // use opencv::prelude::Mat;
+use opencv::imgcodecs::imread;
 
-// pub struct OCR{}
+pub struct OCR {}
 
+impl OCR {
+    /** 二值化 */
+    pub fn binarization() {
+        // 读取图片
+        let img = imread("ocr-test/hrl.jpg", 1).unwrap();
+    }
 
+    /** 拿着二值的图片调用ocr */
+    pub fn ocr() {}
 
-// impl OCR {
-//     /** 二值化 */
-//     pub fn binarization(img:Mat){
-        
-//     }
+    /** 拿着ocr的结果调用get_pet_info */
+    pub fn get_pet_info() {}
 
-//     /** 拿着二值的图片调用ocr */
-//     pub fn ocr(){}
-
-//     /** 拿着ocr的结果调用get_pet_info */
-//     pub fn get_pet_info(){}
-
-//     /** 拿着ocr的结果调用get_stats_info  */
-//     pub fn get_stats_info(){}
-// }
+    /** 拿着ocr的结果调用get_stats_info  */
+    pub fn get_stats_info() {}
+}
 
 #[cfg(test)]
 mod tests {
@@ -30,7 +30,6 @@ mod tests {
         let img = imread("ocr-test/hrl.jpg", 1).unwrap();
 
         // 二值化
-        
 
         println!("{:?}", img);
     }
